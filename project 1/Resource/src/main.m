b2ed0722871c6140fe2314ead710545f40d2f326
awinder0230 <awinder0230@gmail.com>
2017-04-06 13:53:14 +0800

    main

--- project 1/Resource/src/main.m
+++ project 1/Resource/src/main.m
@@ -20,7 +20,7 @@ default_apply_MTB       = true;
 
 
 %% read in images
-fid = fopen('../test_image/image_list.txt', 'r');
+fid = fopen('../input_image/image_list.txt', 'r');
 image_num = str2double(fgets(fid)); % read in first line
 % initialize
 images = [];
@@ -29,13 +29,13 @@ shutter_speed = zeros(image_num, 1);
 for i = 1:image_num
   readline = fgets(fid);
   readline = strsplit(readline);
-  img = imread(strcat('../test_image/',readline{1,1})); % read image 
+  img = imread(strcat('../input_image/',readline{1,1})); % read image 
   shutter_speed(i,1) = str2double(readline{1,2}); % read shutter speed
   images = cat(4, images, img);
 end
 
 %% image alignment
-% initialization
+aligned_images = [];
 reference_id = round(size(images,4)/2);
 if default_image_alignment && default_apply_MTB
   [offset_X, offset_Y] = MTB(images, reference_id);
@@ -44,7 +44,13 @@ else
   offset_Y = zeros(size(images,1), 1);  
 end
 
+for i = 1:image_num
+  aligned_images = cat(4, aligned_images, imtranslate(images(:,:,:,i), [offset_X(i,1), offset_Y(i,1)]));
+end
+
+% image cropping ( not implemented yet! )
+
 %% Test
 for i = 1:image_num
-  imshow(images(:,:,:,i)); 
+  imshow(images(:,:,:,i));
 end
