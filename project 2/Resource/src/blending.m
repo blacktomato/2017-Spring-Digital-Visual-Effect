270b049986ba832e4ee28f15d480c177108fec35
awinder0230 <awinder0230@gmail.com>
2017-05-14 15:38:34 +0800

    bug fix

--- project 2/Resource/src/blending.m
+++ project 2/Resource/src/blending.m
@@ -10,13 +10,13 @@
 
 function images_out = blending(images_in, images_starting_x, images_starting_y, type)
   %% init
-  m = size(images_in,1);
-  n = size(images_in,2);
-  images_out = zeros(max(images_starting_x)+m-1, max(images_starting_y)+n-1, 3);
+  m = size(images_in{1,1,1,1},1);
+  n = size(images_in{1,1,1,1},2);
+  images_out = zeros(max(images_starting_y)+m-1, max(images_starting_x)+n-1, 3);
   for i = 1:m
     for j = 1:n
       for k = 1:3
-        images_out(i,j,k) = images_in(i,j,k,1);
+        images_out(i,j,k) = images_in{1,1,1,1}(i,j,k);
       end
     end
   end
@@ -25,20 +25,20 @@ function images_out = blending(images_in, images_starting_x, images_starting_y, 
   if type == 1
     for i = 2:size(images_in,4)
       left_most_x  = images_starting_x(i);
-      right_most_x = images_starting_x(i-1) + m - 1;
+      right_most_x = images_starting_x(i-1) + n - 1;
       left_image_weight  = linspace(1,0,right_most_x-left_most_x+1);
       right_image_weight = linspace(0,1,right_most_x-left_most_x+1);
-      for a = 1:m
-        for b = 1:n
+      for a = 1:n
+        for b = 1:m
           for c = 1:3
             global_x = images_starting_x(i) + a - 1;
             global_y = images_starting_y(i) + b - 1;
             if (global_x) < right_most_x
               images_out(global_x,global_y,c) ...
                 = images_out(global_x,global_y,c) * left_image_weight(global_x-left_most_x+1) ...
-                + images_in(a,b,c) * right_image_weight(a);
+                + images_in{1,1,1,i}(a,b,c) * right_image_weight(a);
             else
-              images_out(global_x, global_y,c) = images_in(a,b,c);
+              images_out(global_x, global_y,c) = images_in{1,1,1,i}(a,b,c);
             end
           end
         end
