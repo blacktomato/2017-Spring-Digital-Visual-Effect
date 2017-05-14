b60f4d8a525bb78a0f8df4f024691ee97b292b53
blacktomato <s90169@gmail.com>
2017-05-14 13:39:09 +0800

    adding the feature point projection

--- project 2/Resource/src/cylindrical_projection.m
+++ project 2/Resource/src/cylindrical_projection.m
@@ -6,15 +6,19 @@
 %     n: # column
 %     3: rgb three channels
 %     N: number of image
-%   f: focal length for each image in the image (N x 1)
+%   f: focal length for each image in the images (N x 1)
+%   f_p: feature points for each image in the images (N x 1 cell)
 
 % Return:
 %   projected_images: images after operated cylindrical projection (m x n x 3 x N)
 %   masks: the region of the projected images (m x n x N logical array)
+%   projected_f_p: feature points after operated cylindrical projection (N x 1 cell)
 
-function [projected_images, masks] = cylindrical_projection( images, f )
+function [projected_images, masks, projected_f_p] = cylindrical_projection( images, f, f_p)
+
     projected_images = [];
     masks = [];
+    projected_f_p = {};
     image_num = size(images, 4);
     for i = 1:image_num
         % find the transformed boundary
@@ -62,5 +66,16 @@ function [projected_images, masks] = cylindrical_projection( images, f )
         end
         masks = cat(3, masks, mask);
         projected_images = cat(4, projected_images, p_img);
+        
+        if ~exist('f_p')
+            continue;
+        else
+            projected_f_p{i} = f_p{i} - floor([s(2) s(1)] ./ 2);
+            h = sqrt(projected_f_p{i}(:, 1) .^ 2 + f(i) .^ 2);
+            projected_f_p{i}(:, 1)= f(i) * atan(projected_f_p{i}(:, 1) ./ f(i));
+            projected_f_p{i}(:, 2)= f(i) * projected_f_p{i}(:, 2) ./ h;
+            projected_f_p{i} = projected_f_p{i} + floor([s(2) s(1)] ./ 2);
+        end
+
     end
 end
