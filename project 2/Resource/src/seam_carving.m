10fbc6181bc3c2358b242506d44b48a7be44358c
awinder0230 <awinder0230@gmail.com>
2017-05-16 00:29:05 +0800

    seam carving with bug

--- project 2/Resource/src/seam_carving.m
+++ project 2/Resource/src/seam_carving.m
@@ -19,10 +19,18 @@ function image_out = seam_carving(image_in)
   left  = 3;
   
   counter = 0;
+  upper_done = false;
   
   while(true)
+    % search upper boundary of lower boundary
+    if upper_done == false
+      boundary = 1;
+    else
+      boundary = size(image_in,1);
+    end
+    
     % search for upper horizontal boundary segment
-    fprintf('\nSearching boundary segment...\n')
+    % fprintf('\nSearching boundary segment...\n')
     x_start = 0;
     x_end = 0;
     started = false;
@@ -30,7 +38,9 @@ function image_out = seam_carving(image_in)
     x_end_tmp = 0;
     longest_segment = 0;
     for i = 1:size(image_in,2)
-      if (image_in(1,i,1) == -1) && (image_in(1,i,2) == -1) && (image_in(1,i,3) == -1)
+      if (image_in(boundary,i,1) == -1) && ...
+         (image_in(boundary,i,2) == -1) && ...
+         (image_in(boundary,i,3) == -1)
         if (started == false)
           x_start_tmp = i;
           x_end_tmp = i;
@@ -57,17 +67,23 @@ function image_out = seam_carving(image_in)
     
     % no more boundary segment
     if (longest_segment == 0)
-      fprintf('\nNo more boundary segment...\n')
-      break;
+      if upper_done == false
+        upper_done = true;
+        fprintf('\nUpper boundary done, turn for lower boundary\n');
+        continue;
+      else
+        fprintf('\nNo more boundary segment...\n')
+        break;
+      end
     end
     
-    fprintf('\nConverting into black and white image...\n')
+    % fprintf('\nConverting into black and white image...\n')
     % turn colorful image into black and white by equation in MTB
     grey = 54/256  .* image_in(:,x_start:x_end,1) ...
          + 183/256 .* image_in(:,x_start:x_end,2) ...
          + 19/256  .* image_in(:,x_start:x_end,3);
     
-    fprintf('\nComputing gradient...\n')
+    % fprintf('\nComputing gradient...\n')
     % compute gradient using default Sobel's method
     [~, Gmag] = imgradientxy(grey);
     
@@ -86,7 +102,7 @@ function image_out = seam_carving(image_in)
     % init directional map
     Dmap = zeros(size(Gmag,1), size(Gmag,2));
     
-    fprintf('\nDynamic Programming...\n')
+    % fprintf('\nDynamic Programming...\n')
     % dynamic programming
     % start from 2nd column 
     % M(i, j) = e(i, j) + min(M(i?1, j?1),M(i, j-1),M(i+1, j-1))
@@ -135,30 +151,48 @@ function image_out = seam_carving(image_in)
       end
     end
     
-    if (smallest_id == 0) || (smallest_id == 1)
-      fprintf('\nError: should not come here!...\n')
-      break; % didn't find ( this should never happen )
-    end
-    
     for col = x_end:-1:x_start
-      for row = 2:size(Gmag,1)
+      if upper_done == false
+        for row = 2:size(Gmag,1)
           
-        % shift image
-        if row < smallest_id
-          image_in(row-1,col,:) = image_in(row,col,:);
-        elseif (row == smallest_id) && (row == size(Gmag,1)) %% last row
-          image_in(row-1,col,:) = image_in(row,col,:);
-        elseif (row == smallest_id) && (row ~= size(Gmag,1)) %% not last row
-          tmp = image_in(row-1,col,:);
-          image_in(row-1,col,:) = image_in(row,col,:);
-          image_in(row,col,:) = 0.5 .* (tmp + image_in(row+1,col,:));
-        end
+          % shift image
+          if row < smallest_id
+            image_in(row-1,col,:) = image_in(row,col,:);
+          elseif (row == smallest_id) && (row == size(Gmag,1)) %% last row
+            image_in(row-1,col,:) = image_in(row,col,:);
+          elseif (row == smallest_id) && (row ~= size(Gmag,1)) %% not last row
+            tmp = image_in(row-1,col,:);
+            image_in(row-1,col,:) = image_in(row,col,:);
+            image_in(row,col,:) = 0.5 .* (tmp + image_in(row+1,col,:));
+          end
         
-        % update smallest_id
-        if smallest_id == up
-          smallest_id = smallest_id - 1;
-        elseif smallest_id == down
-          smallest_id = smallest_id + 1;
+          % update smallest_id
+          if smallest_id == up
+            smallest_id = smallest_id - 1;
+          elseif smallest_id == down
+            smallest_id = smallest_id + 1;
+          end
+        end
+      else % lower boundary
+        for row = 1:size(Gmag,1)-1
+          
+          % shift image
+          if row > smallest_id
+            image_in(row+1,col,:) = image_in(row,col,:);
+          elseif (row == smallest_id) && (row == 1) %% first row
+            image_in(row+1,col,:) = image_in(row,col,:);
+          elseif (row == smallest_id) && (row ~= 1) %% not first row
+            tmp = image_in(row+1,col,:);
+            image_in(row+1,col,:) = image_in(row,col,:);
+            image_in(row,col,:) = 0.5 .* (tmp + image_in(row-1,col,:));
+          end
+        
+          % update smallest_id
+          if smallest_id == up
+            smallest_id = smallest_id - 1;
+          elseif smallest_id == down
+            smallest_id = smallest_id + 1;
+          end
         end
       end
     end  
